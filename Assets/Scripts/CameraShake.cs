fbc0be7cf5b6d4c4f8796f57bb80fceeb4fd4466
smatphone <jb5982@naver.com>
2020-11-22 23:13:17 +0900

    카메라 흔들림 추가

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float shakeAmount = 5;
+    public float shakeTime = 5;
+    Vector3 initPosition;
+    void Start()
+    {
+        initPosition = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shakeTime > 0)
+        {
+            transform.position = Random.insideUnitSphere * shakeAmount + initPosition;
+            shakeTime -= Time.deltaTime;
+        }
+        else
+        {
+            shakeTime = 0;
+            transform.position = initPosition;
+        }
+    }
+}
